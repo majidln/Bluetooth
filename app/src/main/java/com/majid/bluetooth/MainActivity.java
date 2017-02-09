a5f40d137e4b2112ea38b5a0a7e1971dcaad1be6
majidln <majidlotfinia12@yahoo.com>
2017-02-09 04:44:53 +0330

    add permission and request to turn on blue tooth

--- app/src/main/java/com/majid/bluetooth/MainActivity.java
+++ app/src/main/java/com/majid/bluetooth/MainActivity.java
@@ -1,15 +1,41 @@
 package com.majid.bluetooth;
 
+import android.bluetooth.BluetoothAdapter;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+
+    BluetoothAdapter btAdapter ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        btAdapter = BluetoothAdapter.getDefaultAdapter();
+
+        // Phone does not support Bluetooth so let the user know and exit.
+        if (btAdapter == null) {
+            new AlertDialog.Builder(this)
+                    .setTitle("Not compatible")
+                    .setMessage("Your phone does not support Bluetooth")
+                    .setPositiveButton("Exit", new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int which) {
+                            System.exit(0);
+                        }
+                    })
+                    .setIcon(android.R.drawable.ic_dialog_alert)
+                    .show();
+        }
+
+        if (!btAdapter.isEnabled()) {
+            Intent enableBT = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableBT, BluetoothAdapter.STATE_TURNING_ON);
+        }
     }
 
 }
